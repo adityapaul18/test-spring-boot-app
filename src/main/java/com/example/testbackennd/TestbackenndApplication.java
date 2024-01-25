ed4f36870f6246a4bdddc6e32b8e5b185306ec03
aditya paul <aditya.paul18@gmail.com>
2024-01-25 22:31:44 +0530

    first spring boot tester app code

---
+++ src/main/java/com/example/testbackennd/TestbackenndApplication.java
@@ -0,0 +1,12 @@
+package com.example.testbackennd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestbackenndApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TestbackenndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/testbackennd/TestbackenndApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.testbackennd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestbackenndApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("test ran");
+	}
+
+}
