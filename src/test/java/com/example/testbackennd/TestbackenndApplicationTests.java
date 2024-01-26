a5858355e8c5700e88303a093e32ce5d3ae9a87a
aditya paul <aditya.paul18@gmail.com>
2024-01-26 14:50:02 +0530

    first test endpoints and services

--- src/test/java/com/example/testbackennd/TestbackenndApplicationTests.java
+++ src/test/java/com/example/testbackennd/TestbackenndApplicationTests.java
@@ -3,12 +3,20 @@ package com.example.testbackennd;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Fail.fail;
+
 @SpringBootTest
 class TestbackenndApplicationTests {
 
 	@Test
 	void contextLoads() {
 		System.out.println("test ran");
+	}
+
+	@Test
+	void tst2(){
+		System.out.println("message 2");
+//		fail("fail test message");
 	}
 
 }
