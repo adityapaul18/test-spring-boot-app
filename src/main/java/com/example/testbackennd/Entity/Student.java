7526f177b4fa8b7a4e52f2ffc8059aa825d98e3a
aditya paul <aditya.paul18@gmail.com>
2024-01-27 17:04:18 +0530

    bug resolve in getall and delete cause off collection typo

--- src/main/java/com/example/testbackennd/Entity/Student.java
+++ src/main/java/com/example/testbackennd/Entity/Student.java
@@ -3,7 +3,7 @@ package com.example.testbackennd.Entity;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collation = "students")
+@Document(collection = "student")
 public class Student {
     @Id
     private String _id;
